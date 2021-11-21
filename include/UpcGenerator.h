550f155dc4e858453b16de26506db776a6f1f5bb
nburmaso <nazar.burmasov@cern.ch>
2021-11-21 19:37:01 +0100

    Add a proper license file and license notice

--- include/UpcGenerator.h
+++ include/UpcGenerator.h
@@ -1,6 +1,25 @@
+//////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2021, Nazar Burmasov, Evgeny Kryshen
 //
-// created by Nazar Burmasov on 6/25/21.
+// E-mail of the corresponding author: nazar.burmasov@cern.ch
 //
+// This file is a part of Upcgen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//////////////////////////////////////////////////////////////////////////
+
+// main class of the generator
 
 #ifndef UPCGENERATOR__UPCGENERATOR_H_
 #define UPCGENERATOR__UPCGENERATOR_H_
